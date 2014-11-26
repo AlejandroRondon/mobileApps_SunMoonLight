c4d8517d86a21a5886b6ca53531ebd501a291cf3
AlejandroRondon <rondon_udea@hotmail.com>
2014-11-26 13:59:40 -0500

    Final Version

--- SunMoonLight/gen/com/Blackrondonapps/sunmoonlight/R.java
+++ SunMoonLight/gen/com/Blackrondonapps/sunmoonlight/R.java
@@ -38,9 +38,10 @@ public final class R {
     }
     public static final class drawable {
         public static final int fondo=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
-        public static final int moon=0x7f020002;
-        public static final int sun=0x7f020003;
+        public static final int fondonight=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
+        public static final int moon=0x7f020003;
+        public static final int sun=0x7f020004;
     }
     public static final class id {
         public static final int action_settings=0x7f0a0001;
